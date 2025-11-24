255696e0a052739e9dba927df3147fa24a5bcda8
Abdelghani Belgaid <72890326+abdelghanibelgaid@users.noreply.github.com>
2025-11-24 16:46:15 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (ROOT / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="agrs",
-    version="0.1.2",
+    version="0.1.3",
     description="AGRS: Agricultural Remote Sensing Library",
     long_description=README,
     long_description_content_type="text/markdown",
